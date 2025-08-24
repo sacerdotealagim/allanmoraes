0f4d31a032788b1e48390b38f7b23c8841eaf6e7
sacerdotealagim <Allan-moraes2011@live.com>
2025-08-24 14:09:01 -0300

    Add files via upload

---
+++ allagin.js
@@ -0,0 +1,7 @@
+// menu hamburger
+const menuBtn = document.getElementById('menu-btn');
+const navbar = document.querySelector('.navbar');
+
+menuBtn.addEventListener('click', () => {
+    navbar.classList.toggle('active');
+});
